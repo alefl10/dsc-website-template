0f4f99209d6772ab944efcdeeec49d3c6988aea5
Alejandro Ferrero <alejandroferrero@outlook.com>
2020-10-22 19:59:01 +0200

    Fixed double quote issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/dsc-website-template",
+  pathPrefix: `/dsc-website-template`,
   siteMetadata: {
     title: `Developer Student Club`,
     description: `Developer Student Clubs(DSC) is a Google Developers program for university students to acquire technical skills and solve problems`,
